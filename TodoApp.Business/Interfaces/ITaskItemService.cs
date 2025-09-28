d857ea049d15937e5dc841e0ae1f44f96f8891b4
Mustafa Ozcan <mustafa_ozcan_@hotmail.com>
2025-09-28 21:13:29 +0300

    Service layer methods

---
+++ TodoApp.Business/Interfaces/ITaskItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Entities;
+
+namespace TodoApp.Business.Interfaces
+{
+    public interface ITaskItemService
+    {
+        Task<IEnumerable<TaskItem>> GetAllTasksAsync();
+        Task<TaskItem?> GetTaskByIdAsync(int id);
+        Task AddTaskAsync(TaskItem task);
+        Task UpdateTaskAsync(TaskItem task);
+        Task DeleteTaskAsync(int id);
+        Task<IEnumerable<TaskItem>> GetCompletedTasksAsync();
+    }
+}
---
+++ TodoApp.Business/Services/TaskItemService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Business.Interfaces;
+using TodoApp.DataAccess.Interfaces;
+using TodoApp.Entities;
+
+namespace TodoApp.Business.Services
+{
+    public class TaskItemService : ITaskItemService
+    {
+        private readonly ITaskItemRepository _taskItemRepository;
+        public TaskItemService(ITaskItemRepository taskItemRepository)
+        {
+            _taskItemRepository = taskItemRepository;
+        }
+
+        public async Task AddTaskAsync(TaskItem task)
+        {
+            await _taskItemRepository.AddAsync(task);
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            await _taskItemRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            return await _taskItemRepository.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetCompletedTasksAsync()
+        {
+            return await _taskItemRepository.GetCompletedAsync();
+        }
+
+        public async Task<TaskItem?> GetTaskByIdAsync(int id)
+        {
+            return await _taskItemRepository.GetByIdAsync(id);
+        }
+
+        public async Task UpdateTaskAsync(TaskItem task)
+        {
+            await _taskItemRepository.UpdateAsync(task);
+        }
+    }
+}
